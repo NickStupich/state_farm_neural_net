a4aebb3d362f6d57e5008702d913eb2ca4c21fbb
Nick Stupich <nick.stupich@prolucid.ca>
2016-05-10 14:58:55 -0400

    comparing error to PCA

--- keras_convautoencoder/conv_autoencoder.py
+++ keras_convautoencoder/conv_autoencoder.py
@@ -7,6 +7,8 @@ from keras.layers.core import Dense, Activation, Dropout, Flatten, Reshape
 from autoencoder_layers import DependentDense, Deconvolution2D, DePool2D
 from helpers import show_representations
 
+from sklearn.decomposition import PCA
+from sklearn.metrics import mean_squared_error
 
 def load_data():
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -19,41 +21,82 @@ def load_data():
     return (X_train, y_train), (X_test, y_test)
 
 
-def build_model(nb_filters=32, nb_pool=2, nb_conv=3):
+def build_model(nb_filters=1, nb_pool=2, nb_conv=3):
     model = models.Sequential()
-    #d = Dense(30)
-    c = Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same', input_shape=(1, 28, 28))
+    d = Dense(30, activation='tanh', init='he_normal')
+    #c = Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same', input_shape=(1, 28, 28), activation="sigmoid")
     #mp = MaxPooling2D(pool_size=(nb_pool, nb_pool))
     # =========      ENCODER     ========================
-    model.add(c)
+    #model.add(c)
     #model.add(Activation('tanh'))
     #model.add(mp)
     #model.add(Dropout(0.25))
     # =========      BOTTLENECK     ======================
-    #model.add(Flatten())
-    #model.add(d)
+    model.add(Flatten(input_shape=(1, 28, 28)))
+    model.add(d)
     #model.add(Activation('tanh'))
     # =========      BOTTLENECK^-1   =====================
-    #model.add(DependentDense(nb_filters * 14 * 14, d))
+    model.add(Dense(nb_filters * 28 * 28, activation='tanh',  init='he_normal'))
     #model.add(Activation('tanh'))
-    #model.add(Reshape((nb_filters, 14, 14)))
+    model.add(Reshape((nb_filters, 28, 28)))
     # =========      DECODER     =========================
     #model.add(DePool2D(mp, size=(nb_pool, nb_pool)))
-    model.add(Deconvolution2D(c, border_mode='same'))
+    #model.add(Deconvolution2D(c, border_mode='same', activation="sigmoid"))
     
     #model.add(Activation('tanh'))
 
     return model
 
+def build_model_original(nb_filters=32, nb_pool=2, nb_conv=3):
+    model = models.Sequential()
+    d = Dense(30)
+    c = Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same', input_shape=(1, 28, 28))
+    mp =MaxPooling2D(pool_size=(nb_pool, nb_pool))
+    # =========      ENCODER     ========================
+    model.add(c)
+    model.add(Activation('tanh'))
+    model.add(mp)
+    #model.add(Dropout(0.25))
+    # =========      BOTTLENECK     ======================
+    model.add(Flatten())
+    model.add(d)
+    model.add(Activation('tanh'))
+    # =========      BOTTLENECK^-1   =====================
+    model.add(DependentDense(nb_filters * 14 * 14, d))
+    model.add(Activation('tanh'))
+    model.add(Reshape((nb_filters, 14, 14)))
+    # =========      DECODER     =========================
+    model.add(DePool2D(mp, size=(nb_pool, nb_pool)))
+    model.add(Deconvolution2D(c, border_mode='same'))
+    model.add(Activation('tanh'))
+
+    return model
 
 if __name__ == '__main__':
     (X_train, y_train), (X_test, y_test) = load_data()
-    model = build_model()
+    print(X_train.shape)
+    X_train_flat = np.reshape(X_train, (X_train.shape[0], -1))
+    print(X_train_flat.shape)
+    pca = PCA(n_components = 30)
+    train_components = pca.fit_transform(X_train_flat)
+    print(pca.explained_variance_ratio_)
+    print(train_components.shape)
+    X_train_reconstruction = pca.inverse_transform(train_components)
+    print(X_train_reconstruction.shape)
+    print(X_train_flat.shape)
+    print(X_train_flat[0,:20])
+    print(X_train_reconstruction[0, :20])
+    mse = mean_squared_error(X_train_flat, X_train_reconstruction)
+    print('pca mse (train): %s' % mse)
+
+
+    # model = build_model()
+    model = build_model_original()
     if not False:
         model.compile(optimizer='rmsprop', loss='mean_squared_error')
         model.summary()
         model.fit(X_train, X_train, nb_epoch=200, batch_size=512, validation_split=0.2,
-                  callbacks=[EarlyStopping(patience=3)])
+                  callbacks=[EarlyStopping(patience=20)])
         model.save_weights('./conv.neuro', overwrite=True)
     else:
         model.load_weights('./conv.neuro')
