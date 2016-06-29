wrote scirpt to average prediction csv files averaged 6 vgg16 runs with no driver splitting to get ~0.37 fixing vgg16 cross validation to split by driver

pretrained_vgg16.py
@@ -368,124 +368,90 @@ def vgg_std16_model(img_rows, img_cols, color_type=1):
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+def run_cross_validation(nfolds=10, nb_epoch=10, modelStr=''):
 
-def run_cross_validation(nfolds=10, nb_epoch=10, split=0.2, modelStr=''):
-
-    # Now it loads color image
-    # input image dimensions
     img_rows, img_cols = 224, 224
-    batch_size = 2
+    batch_size = 16
     random_state = 20
-
-    # ishuf_train_data = []
-    # shuf_train_target = []
-    # index_shuf = range(len(train_target))
-    # shuffle(index_shuf)
-    # for i in index_shuf:
-    #     shuf_train_data.append(train_data[i])
-    #     shuf_train_target.append(train_target[i])
-
-    # yfull_train = dict()
-    # yfull_test = []
     num_fold = 0
-    #kf = KFold(len(unique_drivers), n_folds=nfolds,
-               #shuffle=True, random_state=random_state)
-    #for train_drivers, test_drivers in kf:
-    if 0:
-        train_data, train_target, driver_id, unique_drivers = \
-            read_and_normalize_and_shuffle_train_data(img_rows, img_cols,
-                                                      color_type_global, shuffle=True)
-        print('training data mean: %f' % np.mean(train_data))
 
-        for num_fold in range(nfolds):
-	#    num_fold += 1
-            print('Start KFold number {} from {}'.format(num_fold, nfolds))
-            # print('Split train: ', len(X_train), len(Y_train))
-            # print('Split valid: ', len(X_valid), len(Y_valid))
-            # print('Train drivers: ', unique_list_train)
-            # print('Test drivers: ', unique_list_valid)
-            # model = create_model_v1(img_rows, img_cols, color_type_global)
-            # model = vgg_bn_model(img_rows, img_cols, color_type_global)
-            # model = get_trained_vgg16_model_2(img_rows, img_cols, color_type_global)
-            model = vgg_std16_model(img_rows, img_cols, color_type_global)
-            #model = vgg_std16_model2(img_rows, img_cols, color_type_global)
-            
-            weights_path = 'vgg16_full_models/fold%d.h5' % num_fold
+    train_data, train_target, driver_id, unique_drivers = \
+        read_and_normalize_and_shuffle_train_data(img_rows, img_cols,
+                                                  color_type_global, shuffle=False)
 
-            callbacks = []
-            callbacks.append(EarlyStopping(monitor='val_loss', patience=2, verbose=0))
-            callbacks.append(ModelCheckpoint(weights_path, monitor='val_loss', save_best_only=True, verbose=0))
-		
-            
-            model.fit(train_data, train_target, 
-                    batch_size=batch_size,
-                      nb_epoch=nb_epoch,
-                      verbose=1,
-                      validation_split=split, 
-                      shuffle=True,
-                      callbacks=callbacks)
+    kf = KFold(len(unique_drivers), n_folds=nfolds, shuffle=True, random_state=random_state)
+    
+    for num_fold in range(nfolds):
+        model = vgg_std16_model2(img_rows, img_cols, color_type_global)
 
-            # print('losses: ' + hist.history.losses[-1])
+        unique_list_train = [unique_drivers[i] for i in train_drivers]
+        X_train, Y_train, train_index = copy_selected_drivers(train_data, train_target, driver_id, unique_list_train)
+        unique_list_valid = [unique_drivers[i] for i in test_drivers]
+        X_valid, Y_valid, test_index = copy_selected_drivers(train_data, train_target, driver_id, unique_list_valid)
 
-            # print('Score log_loss: ', score[0])
+        num_fold += 1
+        print('Start KFold number {} from {}'.format(num_fold, nfolds))
+        print('Split train: ', len(X_train), len(Y_train))
+        print('Split valid: ', len(X_valid), len(Y_valid))
+        print('Train drivers: ', unique_list_train)
+        print('Test drivers: ', unique_list_valid)
 
-            save_model(model, num_fold, modelStr)
+        
+        weights_path = 'vgg16_xval_models/fold%d.h5' % num_fold
 
-            # predictions_valid = model.predict(X_valid, batch_size=128, verbose=1)
-            # score = log_loss(Y_valid, predictions_valid)
-            # print('Score log_loss: ', score)
-            # Store valid predictions
-            # for i in range(len(test_index)):
-            #    yfull_train[test_index[i]] = predictions_valid[i]
+        callbacks = []
+        callbacks.append(EarlyStopping(monitor='val_loss', verbose=0))
+        callbacks.append(ModelCheckpoint(weights_path, monitor='val_loss', save_best_only=True, verbose=0))
+	
+        
+        model.fit(X_train, Y_train, 
+                batch_size=batch_size,
+                  nb_epoch=nb_epoch,
+                  verbose=1,
+                  validation_data=(X_valid, Y_valid),
+                  shuffle=True,
+                  callbacks=callbacks)
+
+        save_model(model, num_fold, modelStr)
+
+
 
     print('Start testing............')
 
-    if 0:
-        test_data, test_id = read_and_normalize_test_data(img_rows, img_cols,
-                                                          color_type_global)
-        yfull_test = []
-
-        for index in range(1, num_fold + 1):
-            # 1,2,3,4,5
-            # Store test predictions
-            model = read_model(index, modelStr)
-            test_prediction = model.predict(test_data, batch_size=2, verbose=1)
-            yfull_test.append(test_prediction)
-    else:
-        cache_path = os.path.join('cache', 'test_r_' + str(img_rows) +
-                '_c_' + str(img_cols) + '_t_' +
-                str(color_type_global) + '.dat')
-        _, test_id = restore_data(cache_path)
+    cache_path = os.path.join('cache', 'test_r_' + str(img_rows) +
+            '_c_' + str(img_cols) + '_t_' +
+            str(color_type_global) + '.dat')
+    _, test_id = restore_data(cache_path)
 
 
-        yfull_test = np.zeros((nfolds, len(test_id), 10))
-        print('yfull_test shape: %s' % str(yfull_test.shape))
+    yfull_test = np.zeros((nfolds, len(test_id), 10))
+    print('yfull_test shape: %s' % str(yfull_test.shape))
 
-        models = []
+    models = []
+    for index in range(nfolds):
+        model = read_model(index, modelStr)
+        model.summary()
+        model.compile(optimizer='sgd', loss='categorical_crossentropy')
+        models.append(model)
+
+    print('loaded %d models' % len(models))
+
+    splits = 5
+    n = 79726
+    for split in range(splits):
+        print('running split %d' % split)
+        index_range = [int(split*n/splits),int((split+1)*n/splits)]
+        print(index_range)
+
+        split_test_data, split_ids = read_and_normalize_test_data(img_rows, img_cols, color_type_global, index_range = index_range)
+        
+        print('split test data shape: %s' % str(split_test_data.shape))
+
         for index in range(nfolds):
-            model = read_model(index, modelStr)
-            model.summary()
-            model.compile(optimizer='sgd', loss='categorical_crossentropy')
-            models.append(model)
-
-        print('loaded %d models' % len(models))
-
-        splits = 5
-        n = 79726
-        for split in range(splits):
-            print('running split %d' % split)
-            index_range = [int(split*n/splits),int((split+1)*n/splits)]
-            print(index_range)
-
-            split_test_data, split_ids = read_and_normalize_test_data(img_rows, img_cols, color_type_global, index_range = index_range)
-            
-            print('split test data shape: %s' % str(split_test_data.shape))
-
-            for index in range(nfolds):
-                model = models[index]
-                predictions = model.predict(split_test_data, batch_size = 2, verbose=True)
-                print('predictions shape: %s' % str(predictions.shape))
-                yfull_test[index, index_range[0]:index_range[1]] = predictions
+            model = models[index]
+            predictions = model.predict(split_test_data, batch_size = 2, verbose=True)
+            print('predictions shape: %s' % str(predictions.shape))
+            yfull_test[index, index_range[0]:index_range[1]] = predictions
 
 
     info_string = 'loss_' + modelStr \
@@ -500,7 +466,7 @@ def run_cross_validation(nfolds=10, nb_epoch=10, split=0.2, modelStr=''):
 def run_continuous_epochs(nb_epoch=20, val_split=0.1, modelStr=''):
 
     img_rows, img_cols = 224, 224
-    batch_size = 32
+    batch_size = 16
     random_state = 20
 
     train_data, train_target, driver_id, unique_drivers = \
@@ -584,9 +550,9 @@ def test_model_and_submit(start=1, end=1, modelStr=''):
 
 if __name__ == "__main__":
     # nfolds, nb_epoch, split
-    # run_cross_validation(1, 30, 0.1, '_vgg_16_2x20')
+    run_cross_validation(4, 20, '_vgg_16_2x20')
 
-    run_continuous_epochs(modelStr = 'continuous_runs_2')
+    #run_continuous_epochs(modelStr = 'continuous_runs_2')
 
     # nb_epoch, split
     # run_one_fold_cross_validation(10, 0.1)